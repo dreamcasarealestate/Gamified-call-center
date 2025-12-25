0d0fe2ceeec77ffb2450ea284e6c18d5a516c006
sachinchavan11 <dreamcasarealestates@gmail.com>
2025-12-25 21:23:32 +0530

    fixed package.json script

--- envSetup.cjs
+++ envSetup.cjs
@@ -1,17 +1,20 @@
-// envSetup.cjs
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const path = require("node:path");
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const fs = require("node:fs");
 
 const deploymentType = process.env.APP_ENV || "local";
-const sourceEnvPath = path.resolve(process.cwd(), "env", `${deploymentType}.env`);
+const sourceEnvPath = path.resolve(
+  process.cwd(),
+  "env",
+  `${deploymentType}.env`
+);
 const targetEnvPath = path.resolve(process.cwd(), ".env");
 
 try {
   if (!fs.existsSync(sourceEnvPath)) {
     console.error(`❌ Missing env file: ${sourceEnvPath}`);
-    process.exit(1); // IMPORTANT: stop "next dev"
+    process.exit(1);
   }
 
   const data = fs.readFileSync(sourceEnvPath, "utf8");
